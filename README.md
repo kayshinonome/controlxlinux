e9c8bce60553d2941c7ac8d692e1d2f0fa5d8dcb
tsuki-superior <tsukisuperior@tutanota.com>
2021-03-27 23:07:35 -0500

    working

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # controlxlinux
 
 [![C/C++ CI](https://github.com/tsuki-superior/controlxlinux/actions/workflows/compile-test.yml/badge.svg)](https://github.com/tsuki-superior/controlxlinux/actions/workflows/compile-test.yml)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/89f07741b5544bf6b35e696c49c7baea)](https://www.codacy.com/gh/tsuki-superior/controlxlinux/dashboard?utm_source=github.com&utm_medium=referral&utm_content=tsuki-superior/controlxlinux&utm_campaign=Badge_Grade)
 
-A tool to help you make controlx compatible images for the TI-nspie
+A tool to help you make controlx compatible images for the TI-nspire
+
+While I am not responsible for any damage caused to your calculator due to this tool, I can inform you that this tool was indeed tested on real hardware.
+
+## Usage
+
+```sh
+controlxlinux <dtb> <initrd> <kernel> <output_file>
+
+```
--- src/controlxlinux.cpp
+++ src/controlxlinux.cpp
@@ -1,32 +1,70 @@
 #include "controlxlinux.hpp"
 
+using namespace std;
+
 int main(int argc, char **argv)
 {
-	std::ofstream dest;
-	uint32_t dtb_size;
-	uint32_t initrd_size;
-	uint32_t kernel_size;
-	uint32_t buffer[4];
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << "<dtb> <initrd> <kernel>\n";
-		return 1;
-	}
-	dest.open("controlxlinux.img", std::ios::out | std::ios::binary);
-	if (dest.is_open())
-	{
-		std::cerr << "";
-	}
-	dest << header_data;
-	dest << 0xDEDC0FFE;
-	dtb_size = std::filesystem::file_size(argv[1]);
-	initrd_size = std::filesystem::file_size(argv[2]);
-	kernel_size = std::filesystem::file_size(argv[3]);
+  ofstream dest;
+  ifstream dtb_file;
+  ifstream initrd_file;
+  ifstream kernel_file;
 
-	dest << dtb_size << initrd_size << kernel_size;
+  uint32_t dtb_size;
+  uint32_t initrd_size;
+  uint32_t kernel_size;
 
-	append_file(dest, argv[1], dtb_size);
-	append_file(dest, argv[2], initrd_size);
-	append_file(dest, argv[3], kernel_size);
-	fclose(dest);
+  if (argc != 5)
+  {
+    cerr << "Usage: " << argv[0] << " <dtb> <initrd> <kernel> <output_file>\n";
+    return EXIT_FAILURE;
+  }
+
+  dest.open(argv[4], ios::binary);
+  dtb_file.open(argv[1], ios::binary);
+  initrd_file.open(argv[2], ios::binary);
+  kernel_file.open(argv[3], ios::binary);
+
+  if (!dest.is_open())
+  {
+    cerr << "Could not open output file\n";
+    return EXIT_FAILURE;
+  }
+
+  if (!dtb_file.is_open())
+  {
+    cerr << argv[1] << " could not be opened\n";
+    return EXIT_FAILURE;
+  }
+
+  if (!initrd_file.is_open())
+  {
+    cerr << argv[2] << " could not be opened\n";
+    return EXIT_FAILURE;
+  }
+
+  if (!kernel_file.is_open())
+  {
+    cerr << argv[3] << " could not be opened\n";
+    return EXIT_FAILURE;
+  }
+
+  dest.write((char *)&header_data, sizeof(header_data));
+
+  dtb_size = filesystem::file_size(argv[1]);
+  initrd_size = filesystem::file_size(argv[2]);
+  kernel_size = filesystem::file_size(argv[3]);
+
+  dest.write((char *)&dtb_size, sizeof(dtb_size));
+  dest.write((char *)&kernel_size, sizeof(kernel_size));
+  dest.write((char *)&initrd_size, sizeof(initrd_size));
+
+  dest << dtb_file.rdbuf();
+  dest << kernel_file.rdbuf();
+  dest << initrd_file.rdbuf();
+
+  dest.close();
+  dtb_file.close();
+  initrd_file.close();
+  kernel_file.close();
+  return EXIT_SUCCESS;
 }
